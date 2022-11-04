7cb2a38f2a61207bd2bcfbc39e2e2bd2ef55dbcc
hp <sahalnurdin@gmail.com>
2022-11-04 21:24:07 +0700

    add changes

---
+++ task1.js
@@ -0,0 +1,12 @@
+// Assigmment 
+// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+// Answer:
+
+let country = "Indonesia";
+let continent = "Asia";
+let population = 250000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
